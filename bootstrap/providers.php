fe38df8a2bfcfa3811bd48dbf2b74144fe7271a0
Marco "Mint" Introini <mint.dev@pm.me>
2024-04-23 16:09:31 +0200

    Install Filament

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,4 +2,8 @@
 
 return [
     App\Providers\AppServiceProvider::class,
+
+    // Filament
+    App\Providers\Filament\AdminPanelProvider::class,
+    App\Providers\Filament\UserPanelProvider::class,
 ];
