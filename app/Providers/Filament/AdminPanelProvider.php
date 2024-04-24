e855fa47ded06c3b9284206d904325d28f6e3db0
Marco "Mint" Introini <mint.dev@pm.me>
2024-04-24 13:15:17 +0200

    Add Filament local logins and Larastan PHPStan
    Added the "better-futures-studio/filament-local-logins" package for local logins in Filament and updated the configuration accordingly. Also integrated "larastan/larastan" package for static code analysis with PHPStan.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use BetterFuturesStudio\FilamentLocalLogins\LocalLogins;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -17,6 +18,7 @@ use Illuminate\Routing\Middleware\SubstituteBindings;
 use Illuminate\Session\Middleware\AuthenticateSession;
 use Illuminate\Session\Middleware\StartSession;
 use Illuminate\View\Middleware\ShareErrorsFromSession;
+use Jeffgreco13\FilamentBreezy\BreezyCore;
 
 class AdminPanelProvider extends PanelProvider
 {
@@ -53,6 +55,11 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
+            ])
+            ->plugins([
+                new LocalLogins(),
+                BreezyCore::make()
+                    ->myProfile(),
             ]);
     }
 }
