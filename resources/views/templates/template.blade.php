214ae669b17250b19866bdc50eeb4499eb26eb07
Marco "Mint" Introini <mint.dev@pm.me>
2024-05-05 21:25:38 +0200

    Update title and add props in blade template
    The update allows for dynamic title setting in the blade template. 'Props' was added at the beginning to define a 'title' variable that is used to set the HTML title in the page header. A slot was also inserted in the body to allow for insertion of specific content.

--- resources/views/templates/template.blade.php
+++ resources/views/templates/template.blade.php
@@ -1,3 +1,7 @@
+@props([
+    'title' => 'Santo del Giorno'
+])
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -5,9 +9,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    @vite('resources/css/app.css')
+    <title>{{$title}}</title>
 </head>
 <body>
+
+{{$slot}}
 
 </body>
 </html>
