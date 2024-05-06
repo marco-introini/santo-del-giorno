bbae9e68fe8c5e7462db715aa38d11a4abca41d7
Marco "Mint" Introini <mint.dev@pm.me>
2024-05-06 16:10:13 +0200

    Add FonteController

---
+++ app/Http/Controllers/Api/V1/FonteController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api\V1;
+
+use App\Http\Controllers\ApiController;
+use App\Http\Resources\FonteResource;
+use App\Models\Fonte;
+use App\Traits\ApiResponse;
+
+class FonteController extends ApiController
+{
+    use ApiResponse;
+
+    public function show(Fonte $fonte)
+    {
+        return new FonteResource($fonte);
+    }
+}
