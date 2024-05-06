d1cf80490f33eb9891761cbaa3fb885feb1c1a9a
Marco "Mint" Introini <mint.dev@pm.me>
2024-05-06 16:10:35 +0200

    Add basic ApiController for query string parameters

---
+++ app/Http/Controllers/ApiController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ApiController extends Controller
+{
+    public function include(string $relazione): bool
+    {
+        $param = request()->input('include','');
+        $includeValues = explode(',', strtolower($param));
+        return in_array(strtolower($relazione), $includeValues);
+    }
+}
