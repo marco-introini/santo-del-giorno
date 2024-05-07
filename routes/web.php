a5c717055ab143d341c6ed01aef80d199ce4ae5f
Marco "Mint" Introini <mint.dev@pm.me>
2024-05-07 14:31:42 +0200

    Add 'home' name to root route
    The root route in the web.php file has been modified to include a name. This makes referencing the

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})
+->name('home');
 
 Route::view('cerca-per-data', 'cerca-per-data')
     ->name('cerca-per-data');
