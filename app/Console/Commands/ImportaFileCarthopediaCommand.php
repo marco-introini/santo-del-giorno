b4523b5eca39fa8e8999ed84e97c84495108b040
Marco "Mint" Introini <mint.dev@pm.me>
2024-05-10 16:15:29 +0200

    Update ImportaFileCarthopediaCommand to manage file argument
    The code has been updated to allow the importa:file-carthopedia command to accept a filename as an argument. It further contains an error handling mechanism to deal with any issues that may arise while opening the file.

--- app/Console/Commands/ImportaFileCarthopediaCommand.php
+++ app/Console/Commands/ImportaFileCarthopediaCommand.php
@@ -2,16 +2,33 @@
 
 namespace App\Console\Commands;
 
+use Exception;
 use Illuminate\Console\Command;
+
 
 class ImportaFileCarthopediaCommand extends Command
 {
-    protected $signature = 'importa:file-carthopedia';
+    protected $signature = 'importa:file-carthopedia {file}';
 
     protected $description = 'Carica i dati da un file del formato DATA\nNome,Descrizione';
 
     public function handle(): void
     {
+        $file = $this->argument('file');
+
+        $this->info('Caricamento da '.$file);
+
+        try {
+            $file = fopen("percorso/al/tuo/file.txt", "r");
+
+            while (($line = fgets($file)) !== false) {
+                //TODO
+            }
+            fclose($file);
+        }
+        catch (Exception $e) {
+            $this->error('Impossibile aprire il file. Errore: '.$e->getMessage());
+        }
 
     }
 }
