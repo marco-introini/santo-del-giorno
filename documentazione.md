47288c3e1b3eca8fa7b028929c800d859786df62
Marco "Mint" Introini <mint.dev@pm.me>
2024-05-14 15:03:48 +0200

    Updated welcome blade and documentation
    The "welcome" blade view has been updated to reformat the content and enhance readability. Changes have been made to improve indentation and content structure. Additional information has also been included about the project's data sources. The "documentazione.md" was significantly updated to include instructions on how to use the web version and detailed API usage guidelines. A minor update was added to the "ElencoSanti.http" test.

--- documentazione.md
+++ documentazione.md
@@ -1,13 +1,71 @@
 # Documentazione
 
+## Versione WEB
+
+L'utilizzo della versione WEB è molto semplice: basta selezionare la tipologia di ricerca (per data o per nome) ed
+effettuare la ricerca
+
 ## API
+
+Molto più interessante è la possibilità di richiamare il servizio via API.
+
+Per fare questo è necessario registrarsi e confermare la propria mail.
+**La registrazione è assolutamente gratuita e l'indirizzo email non verrà MAI ceduto a nessuno.**
+
+Tutte le chiamate alle API devono essere autenticate usando un Beaer token ottenuto nella propria area personale, alla
+voce **Profilo**, oppure chiamando l'API login
+
+```bash
+curl --location 'https://santodelgiorno.mintdev.me/api/login' \
+--form 'email="user@example.com"' \
+--form 'password="password"'
+```
+
+<img src="/storage/immagini/screen1.png">
+
+### Elenco Santi
 
 Elenco paginato tutti i santi inseriti
 
 ```
-GET /api/v1/santo
+GET https://santodelgiorno.mintdev.me/api/v1/santo
+Accept: application/json
+Authorization: Bearer <token>
 ```
 
-GET /api/santo/{id} torna un singolo santo dato l'ID
-GET /api/santo/nome/{nome} torna il o i santi con nome in %LIKE%
-GET /api/santo/data/{mese}/{giorno} torna il santo del mese specificato
+### Informazione su singolo santo
+
+```
+GET https://santodelgiorno.mintdev.me/api/v1/santo/<uuid>
+Accept: application/json
+Authorization: Bearer <token>
+```
+Per avere il dettaglio della fonte:
+
+```
+GET https://santodelgiorno.mintdev.me/api/v1/santo/<uuid>?include=fonte
+Accept: application/json
+Authorization: Bearer <token>
+```
+
+### Ricerca
+
+#### Ricerca per data
+
+```
+GET https://santodelgiorno.mintdev.me/api/v1/santo/data/{mese}/{giorno}
+Accept: application/json
+Authorization: Bearer <token>
+```
+
+- mese: numero intero tra 1 e 12
+- giorno: numero intero tra 1 e 31
+
+#### Ricerca per nome
+
+```
+GET https://santodelgiorno.mintdev.me/api/v1/santo/nome/{nome}
+Accept: application/json
+Authorization: Bearer <token>
+```
+La ricerca avviene in modo case-insensitive e in LIKE
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,36 +1,45 @@
 <x-template>
 
-                    <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:gap-8">
-                        <div class="">
-                            <img src="/images/logo.webp"
-                                 alt="Santo del giorno"
-                                 class="h-full w-fit flex-1 rounded-[10px] object-top object-cover drop-shadow-[0px_4px_34px_rgba(0,0,0,0.25)] dark:block"
-                            />
-                        </div>
-                        <div class="place-content-center mx-auto">
-                            <div class="font-extrabold text-5xl">Santo del Giorno</div>
-                            <div class="mt-4">
-                                Un progetto di software libero per avere API semplici e complete per ottenere il santo del giorno.
-                            </div>
-                            <div class="mt-2">
-                                Le chiamate alle API possono essere integrate in <strong>qualunque progetto (anche
-                                    commerciale)</strong> in modo <strong>completamente gratuito</strong>.
-                                La registrazione è necessaria per consentire il monitoraggio degli accessi e la gestione della sicurezza
-                            </div>
-                        </div>
-                    </div>
+    <div class="grid grid-cols-1 gap-6 md:grid-cols-2 lg:gap-8">
+        <div class="">
+            <img src="/images/logo.webp"
+                 alt="Santo del giorno"
+                 class="h-full w-fit flex-1 rounded-[10px] object-top object-cover drop-shadow-[0px_4px_34px_rgba(0,0,0,0.25)] dark:block"
+            />
+        </div>
+        <div class="place-content-center mx-auto">
+            <div class="font-extrabold text-5xl">Santo del Giorno</div>
+            <div class="mt-4">
+                Un progetto di software libero per avere API semplici e complete per ottenere il santo del giorno.
+            </div>
+            <div class="mt-2">
+                Le chiamate alle API possono essere integrate in <strong>qualunque progetto (anche
+                    commerciale)</strong> in modo <strong>completamente gratuito</strong>.
+                La registrazione è necessaria per consentire il monitoraggio degli accessi e la gestione della sicurezza
+            </div>
+        </div>
+    </div>
 
-                    <div class="mt-12">
-                        <article class="prose dark:prose-invert max-w-none">
 
-@php
-        $commonmark = new \League\CommonMark\CommonMarkConverter();
-        $file = base_path().'/documentazione.md';
-        $contents = file_get_contents($file);
+    <div class="mt-12">
+        Il progetto prevede l'importazione di più fonti dati. Al momento, anche per motivi di copyright, i dati vengono
+        presi esclusivamente da
+        <a href="https://it.cathopedia.org/wiki/Cathopedia:Pagina_principale" class="text-violet-950 underline hover:text-amber-600" target="_blank">Cathopedia</a>
+        la Wikipedia Cattolica
 
-        echo $commonmark->convert($contents);
-@endphp
-                        </article>
-                    </div>
+    </div>
+
+    <div class="mt-12">
+        <article class="prose dark:prose-invert max-w-none">
+
+            @php
+                $commonmark = new \League\CommonMark\CommonMarkConverter();
+                $file = base_path().'/documentazione.md';
+                $contents = file_get_contents($file);
+
+                echo $commonmark->convert($contents);
+            @endphp
+        </article>
+    </div>
 
 </x-template>
