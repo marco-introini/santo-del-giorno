029bfff9dfdd0bad5b7a77bed832b5bacf086b59
Marco "Mint" Introini <mint.dev@pm.me>
2024-05-14 15:18:22 +0200

    add deploy script

---
+++ _deploy/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Deploy - to be executed as normal user
+
+cd /var/www/layer7-console || exit
+
+php artisan down
+
+sudo chown -R marco:www-data .
+git pull
+composer install --no-interaction --optimize-autoloader --no-dev
+npm install
+npm run build
+
+php artisan optimize:clear
+
+php artisan down
+
+php artisan migrate --force
+php artisan config:cache
+php artisan route:cache
+php artisan filament:upgrade
+
+sudo chown -R www-data:www-data .
+
+php artisan up
---
+++ _deploy/first_deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Must be run as root
+
+# Cloned in /var/www/santodelgiorno
+
+cp nginx.conf /etc/nginx/sites-available/santodelgiorno
+ln -s /etc/nginx/sites-available/santodelgiorno /etc/nginx/sites-enabled/
+service nginx reload
+
+certbot --nginx
+
+chown -R www-data:www-data /var/www/santodelgiorno/
+
+echo "* * * * * www-data cd /var/www/santodelgiorno && php artisan schedule:run >> /dev/null 2>&1" >> /etc/crontab
+
+cd /var/www/santodelgiorno || exit
+php artisan storage:link
