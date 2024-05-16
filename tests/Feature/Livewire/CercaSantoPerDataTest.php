6973be6e54aaf1053ca04d7c0e262cf3cb5a1639
Marco "Mint" Introini <mint.dev@pm.me>
2024-05-16 15:07:32 +0200

    Enhance Livewire test coverage for Santo models
    The Livewire test coverage for Santo models has been expanded with a variety of scenarios accounted for. In addition to verifying the successful rendering of the components, the new tests validate the functionality of searching for a Santo by both name and date, ensuring results match the given parameters.

--- tests/Feature/Livewire/CercaSantoPerDataTest.php
+++ tests/Feature/Livewire/CercaSantoPerDataTest.php
@@ -1,9 +1,54 @@
 <?php
 
 use App\Livewire\CercaSantoPerData;
+use App\Models\Santo;
 use Livewire\Livewire;
 
 it('renders successfully', function () {
     Livewire::test(CercaSantoPerData::class)
         ->assertStatus(200);
+});
+
+test('la ricerca torna il santo di oggi', function () {
+    $santo = Santo::factory()->create([
+        'mese' => now()->month,
+        'giorno' => now()->day,
+    ]);
+
+    Livewire::test(CercaSantoPerData::class)
+        ->set('nome',$santo->nome)
+        ->assertStatus(200)
+        ->assertSee($santo->nome)
+        ->assertSee($santo->giorno)
+        ->assertSee($santo->mese);
+});
+
+test('la ricerca torna il santo di un giorno scelto', function () {
+    $santo = Santo::factory()->create([
+        'mese' => now()->month,
+        'giorno' => now()->day,
+    ]);
+
+    Livewire::test(CercaSantoPerData::class)
+        ->set('giorno',$santo->giorno)
+        ->set('mese',$santo->mese)
+        ->assertStatus(200)
+        ->assertSee($santo->nome)
+        ->assertSet('giorno',$santo->giorno)
+        ->assertSet('mese',$santo->mese);
+});
+
+test('la ricerca torna NON il santo di un altro giorno', function () {
+    $santo = Santo::factory()->create([
+        'mese' => 10,
+        'giorno' => 20,
+    ]);
+
+    Livewire::test(CercaSantoPerData::class)
+        ->set('giorno',21)
+        ->set('mese',11)
+        ->assertStatus(200)
+        ->assertDontSee($santo->nome)
+        ->assertSet('giorno',21)
+        ->assertSet('mese',11);
 });
--- tests/Feature/Livewire/CercaSantoPerNomeTest.php
+++ tests/Feature/Livewire/CercaSantoPerNomeTest.php
@@ -1,9 +1,36 @@
 <?php
 
 use App\Livewire\CercaSantoPerNome;
+use App\Models\Santo;
 use Livewire\Livewire;
 
-it('renders successfully', function () {
+test('mostra un santo', function () {
+    $santo = Santo::factory()->create();
+
     Livewire::test(CercaSantoPerNome::class)
-        ->assertStatus(200);
+        ->assertStatus(200)
+        ->assertSee($santo->nome)
+        ->assertSee($santo->giorno)
+        ->assertSee($santo->mese);
+});
+
+test('la ricerca torna il santo', function () {
+    $santo = Santo::factory()->create();
+
+    Livewire::test(CercaSantoPerNome::class)
+        ->set('nome',$santo->nome)
+        ->assertStatus(200)
+        ->assertSee($santo->nome)
+        ->assertSee($santo->giorno)
+        ->assertSee($santo->mese);
+});
+
+
+test('la ricerca NON torna il santo se la query è diversa', function () {
+    $santo = Santo::factory()->create();
+
+    Livewire::test(CercaSantoPerNome::class)
+        ->set('nome', 'XXX')
+        ->assertStatus(200)
+        ->assertDontSee($santo->nome);
 });
