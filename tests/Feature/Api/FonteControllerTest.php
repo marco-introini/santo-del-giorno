ee4721c5c8a02adab3baf56fd7593fba9bf05a8e
Marco "Mint" Introini <mint.dev@pm.me>
2024-05-17 15:43:29 +0200

    Refactor tests

---
+++ tests/Feature/Api/FonteControllerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Models\Fonte;
+use App\Models\User;
+use function Pest\Laravel\actingAs;
+use function Pest\Laravel\get;
+
+beforeEach(function () {
+   $user = User::factory()->create();
+   actingAs($user);
+});
+
+
+test('a source returns', function (){
+    $fonte = Fonte::factory()->create();
+
+    $response = get(route('fonte.show', $fonte));
+
+    $response->assertStatus(200);
+    expect($response->json('data.type'))->toBe('fonte')
+        ->and($response->json('data.attributes.nome'))->toBe($fonte->nome)
+        ->and($response->json('data.attributes.url'))->toBe($fonte->url);
+});
