0f11a4e6f6bc3dcf357d37b4dfd3084a42fd58c8
Marco "Mint" Introini <mint.dev@pm.me>
2024-06-09 08:27:50 +0200

    Add Onomastico and Onomastico Secondario columns
    Two new columns, "Onomastico" and "Onomastico Secondario", have been added to the 'tabella-santi' view. These columns indicate whether a holiday name day and secondary name day exists for each saint, with a checkmark for a present name day and an x for absent.

--- resources/views/components/tabella-santi.blade.php
+++ resources/views/components/tabella-santi.blade.php
@@ -9,6 +9,8 @@
         <td class="px-3 py-3 text-lg font-bold bg-violet-300">Nome</td>
         <td class="px-3 py-3 text-lg font-bold bg-violet-300">Giorno</td>
         <td class="px-3 py-3 text-lg font-bold bg-violet-300">Mese</td>
+        <td class="px-3 py-3 text-lg font-bold bg-violet-300">Onomastico</td>
+        <td class="px-3 py-3 text-lg font-bold bg-violet-300">Onomastico Secondario</td>
         <td class="px-3 py-3 text-lg font-bold bg-violet-300">Note</td>
     </tr>
     @foreach($santi as $santo)
@@ -16,6 +18,20 @@
             <td class="px-3 py-3">{{$santo->nome}}</td>
             <td class="px-3 py-3">{{$santo->giorno}}</td>
             <td class="px-3 py-3">{{$santo->mese}}</td>
+            <td class="px-3 py-3">
+                @if($santo->onomastico)
+                    <x-heroicon-o-check-circle class="text-green-900 max-w-6 mx-auto"/>
+                @else
+                    <x-heroicon-o-x-circle class="text-red-900 max-w-6 mx-auto"/>
+                @endif
+            </td>
+
+            <td class="px-3 py-3">
+                @if($santo->onomastico_secondario)
+                    <x-heroicon-o-check-circle class="text-green-900 max-w-6 mx-auto"/>
+                @else
+                    <x-heroicon-o-x-circle class="text-red-900 max-w-6 mx-auto"/>
+                @endif</td>
             <td class="px-3 py-3">{{$santo->note}}</td>
         </tr>
     @endforeach
