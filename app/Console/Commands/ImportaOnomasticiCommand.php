ddcd37e877e979f5754e4a37c3b412a83e90e35f
Marco "Mint" Introini <mint.dev@pm.me>
2024-07-01 10:52:17 +0200

    Update ImportaOnomasticiCommand to parse file data
    The ImportaOnomasticiCommand has been updated to accept a file argument and parse it accordingly to create or update records in the database. The source information has been amended, including the name and notes. A dedicated method to convert month names to their numerical representation was added, increasing the efficiency and cleanliness of the class.

--- app/Console/Commands/ImportaOnomasticiCommand.php
+++ app/Console/Commands/ImportaOnomasticiCommand.php
@@ -3,12 +3,13 @@
 namespace App\Console\Commands;
 
 use App\Models\Fonte;
+use App\Models\Santo;
 use Exception;
 use Illuminate\Console\Command;
 
 class ImportaOnomasticiCommand extends Command
 {
-    protected $signature = 'importa:onomastici';
+    protected $signature = 'importa:onomastici {file}';
 
     protected $description = 'Importa onomastici da file preso da https://luirig.altervista.org/calendario/onomast.htm';
 
@@ -19,22 +20,53 @@ class ImportaOnomasticiCommand extends Command
         $this->info('Caricamento da '.$file);
 
         $fonte = Fonte::firstOrCreate([
-            'nome' => 'Cathopedia',
+            'nome' => 'Luigi Rignanese',
             'url' => 'https://luirig.altervista.org/calendario/onomast.htm',
-            'note' => 'Wikipedia Cattolica'
+            'note' => 'Sito web Luigì Rignanese',
         ]);
 
         try {
             $file = fopen($file, "r");
 
-            $giornoAttuale = 0;
-            $meseAttuale = 0;
             while (($line = fgets($file)) !== false) {
                 $line = rtrim($line, "\r\n");
-                fclose($file);
+                $tokens = explode(";", $line);
+                $nome = $tokens[0];
+                $data = explode(' ',$tokens[1]);
+                $giorno = $data[0];
+                $mese = self::ottieniNumeroMese($data[1]);
+
+                $santo = Santo::firstOrCreate([
+                    'fonte_id' => $fonte->id,
+                    'nome' => $nome,
+                    'giorno' => $giorno,
+                    'mese' => $mese,
+                    'onomastico' => true,
+                    'onomastico_secondario' => false,
+                    ]);
             }
+            fclose($file);
         } catch (Exception $e) {
             $this->error('Impossibile aprire il file. Errore: '.$e->getMessage());
         }
+    }
+
+    private static function ottieniNumeroMese(string $monthName): ?int
+    {
+        return match ($monthName) {
+            'gennaio' => 1,
+            'febbraio' => 2,
+            'marzo' => 3,
+            'aprile' => 4,
+            'maggio' => 5,
+            'giugno' => 6,
+            'luglio' => 7,
+            'agosto' => 8,
+            'settembre' => 9,
+            'ottobre' => 10,
+            'novembre' => 11,
+            'dicembre' => 12,
+            default => null,
+        };
     }
 }
