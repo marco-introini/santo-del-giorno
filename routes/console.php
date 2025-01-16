efd3f8b3dda61c2fd424b02d7f52e44466bf3dc1
Marco "Mint" Introini <mint.dev@pm.me>
2025-01-16 12:31:21 +0100

    Add scheduled tasks

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,4 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
-use Illuminate\Support\Facades\Artisan;
-
-Artisan::command('inspire', function (): void {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+Schedule::command('backup:run')
+    ->weeklyOn(1, '03:00');
