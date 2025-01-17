e6922c608136b8e77b364fbf4f3fc9bde43d1815
Marco "Mint" Introini <mint.dev@pm.me>
2025-01-17 16:37:46 +0100

    feat: Replace HTML elements with Flux components
    Updated the UI by replacing standard HTML elements with Flux components to enhance consistency and reusability. Adjusted the structure and styling to improve responsiveness and maintain accessibility.

--- resources/views/livewire/cerca-santo-per-data.blade.php
+++ resources/views/livewire/cerca-santo-per-data.blade.php
@@ -1,43 +1,36 @@
 <div>
 
 
-    <x-titolo>Cerca per Data</x-titolo>
-    <div class="flex flex-row items-center">
-        <div class="mr-3">Mese</div>
-        <select
-            class="px-3 py-1 border-violet-900 border border-solid rounded-xl "
-            name="mese"
-            wire:model.live="mese">
-            @for($i=1;$i<=12;$i++)
-                <option value="{{$i}}">{{$i}}</option>
-            @endfor
-        </select>
-        <div class="ml-10 mr-3">Giorno</div>
-        <select
-            class="px-3 py-1 border-violet-900 border border-solid rounded-xl"
-            name="giorno"
-            wire:model.live="giorno">
-
-            @for($i=1;$i<=31;$i++)
-                <option value="{{$i}}">{{$i}}</option>
-            @endfor
-        </select>
-    </div>
-
-    <div class="mt-4">
-        <x-titolo class="text-xl">Onomastici</x-titolo>
-        <x-titolo class="text-lg">Primario</x-titolo>
-        <ul>
-        @foreach($onomasticiPrimari as $santo)
-            <li>{{$santo->nome}}</li>
-        @endforeach
-        </ul>
-        <x-titolo class="text-lg">Secondari</x-titolo>
-        <ul>
-            @foreach($onomasticiSecondari as $santo)
-                <li>{{$santo->nome}}</li>
-            @endforeach
-        </ul>
+    <flux:heading size="xl">Cerca per Data</flux:heading>
+    <div class="mt-4 flex flex-col md:flex-row gap-8">
+        <div class="flex flex-row items-center max-w-fit gap-4">
+            <flux:select variant="listbox" class="mr-3" wire:model.live="mese" label="Mese">
+                @for($i=1;$i<=12;$i++)
+                    <flux:option>{{$i}}</flux:option>
+                @endfor
+            </flux:select>
+            <flux:select variant="listbox" class="mr-3" wire:model.live="giorno" label="Giorno">
+                @for($i=1;$i<=31;$i++)
+                    <flux:option>{{$i}}</flux:option>
+                @endfor
+            </flux:select>
+        </div>
+        <div class="w-full flex flex-col gap-2">
+            <flux:heading size="lg">Onomastici</flux:heading>
+            <flux:separator />
+            <flux:heading>Primari</flux:heading>
+            <ul>
+                @foreach($onomasticiPrimari as $santo)
+                    <li>{{$santo->nome}}</li>
+                @endforeach
+            </ul>
+            <flux:heading class="mt-2">Secondari</flux:heading>
+            <ul>
+                @foreach($onomasticiSecondari as $santo)
+                    <li>{{$santo->nome}}</li>
+                @endforeach
+            </ul>
+        </div>
     </div>
 
 
