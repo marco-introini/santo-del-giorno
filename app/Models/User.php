c21f0203dcd51c146a41a1dda874d1e1bef6330a
Marco "Mint" Introini <mint.dev@pm.me>
2025-02-19 15:08:01 +0100

    fix phpstan

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,18 +18,8 @@ class User extends Authenticatable implements MustVerifyEmail, FilamentUser, Has
 {
     use HasFactory, Notifiable, HasApiTokens, TwoFactorAuthenticatable;
 
-    /**
-     * The attributes that are mass ASSIGNABLE.
-     *
-     * @var array<int, string>
-     */
     protected $guarded = [];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
