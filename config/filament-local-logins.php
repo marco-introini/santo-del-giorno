fa6bcbb4436354714285a0962618e5fdc8300c13
Marco "Mint" Introini <mint.dev@pm.me>
2025-11-13 09:44:54 +0100

    update

--- config/filament-local-logins.php
+++ config/filament-local-logins.php
@@ -6,12 +6,12 @@ return [
     'panels' => [
         'admin' => [
             'enabled' => env('ADMIN_PANEL_LOCAL_LOGINS_ENABLED', env('APP_ENV') === 'local'),
-            'emails' => array_filter(array_map('trim', explode(',', (string) env('ADMIN_PANEL_LOCAL_LOGIN_EMAILS', '')))),
+            'emails' => array_filter(array_map(trim(...), explode(',', (string) env('ADMIN_PANEL_LOCAL_LOGIN_EMAILS', '')))),
             'login_page' => LoginPage::class,
         ],
         'user' => [
             'enabled' => env('USER_PANEL_LOCAL_LOGINS_ENABLED', env('APP_ENV') === 'local'),
-            'emails' => array_filter(array_map('trim', explode(',', (string) env('USER_PANEL_LOCAL_LOGIN_EMAILS', '')))),
+            'emails' => array_filter(array_map(trim(...), explode(',', (string) env('USER_PANEL_LOCAL_LOGIN_EMAILS', '')))),
             'login_page' => LoginPage::class,
         ],
     ],
